7ab1cee181004b02bfdcf05fe52b5dbbcab5c5e9
Matheus Pereira <mat_pereiraa@hotmail.com>
2022-12-26 15:29:35 -0300

    setup: QR Code internal config

--- src/screens/SignedIn/Qrcode/index.tsx
+++ src/screens/SignedIn/Qrcode/index.tsx
@@ -1,32 +1,49 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Pressable, StatusBar } from 'react-native';
-import { useTheme } from 'styled-components/native';
-import { Icon } from '../../../components/Icon';
-import { Separator } from '../../../components/Separator';
-import { Text } from '../../../components/Text';
+import {useNavigation} from '@react-navigation/native';
+import React, {useMemo} from 'react';
+import {Pressable, StatusBar, useWindowDimensions, ViewStyle} from 'react-native';
+import {useTheme} from 'styled-components/native';
+import {Icon} from '../../../components/Icon';
+import {Separator} from '../../../components/Separator';
+import {Text} from '../../../components/Text';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
 
-import {
- Container
-} from './styles';
+import {Container, Content, Scroll} from './styles';
 
-export function Qrcode(){
-  const {goBack} = useNavigation()
+export function Qrcode() {
+  const {goBack} = useNavigation();
   const {spacing} = useTheme();
+  const {height, width} = useWindowDimensions()
+  const cameraStyle = useMemo(() => ({
+    marginTop: 40,
+    flex: 0,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: width * 0.7,
+    width: width * 0.7,
+  } as ViewStyle),[width, height])
 
-
-return (
-   <Container>
-    <StatusBar barStyle={'dark-content'}/>
-    <Separator height={spacing.md} />
-    <Pressable onPress={goBack}>
-      <Icon icon='close' size={15} />
-    </Pressable>
-    <Separator height={spacing.md} />
-    <Text typography='h7'>Ler QR Code</Text>
-    <Separator height={spacing.sm} />
-    <Text typography='caption'>Posicione o código QR dentro da área</Text>
-    <Separator height={spacing.lg} />
-   </Container>
+  return (
+    <Container>
+      <Scroll>
+        <Content>
+        <StatusBar barStyle={'dark-content'} />
+        <Separator height={spacing.md} />
+        <Pressable onPress={goBack}>
+          <Icon icon="close" size={15} />
+        </Pressable>
+        <Separator height={spacing.md} />
+        <Text typography="h7">Ler QR Code</Text>
+        <Separator height={spacing.sm} />
+        <Text typography="caption">Posicione o código QR dentro da área</Text>
+        <Separator height={spacing.xxxl} />
+        </Content>
+        <QRCodeScanner
+          cameraStyle={cameraStyle}
+          onRead={e => console.log(e.data)}
+          flashMode={RNCamera.Constants.FlashMode.torch}></QRCodeScanner>
+      </Scroll>
+    </Container>
   );
 }
--- src/screens/SignedIn/Qrcode/styles.ts
+++ src/screens/SignedIn/Qrcode/styles.ts
@@ -3,5 +3,14 @@ import styled from 'styled-components/native';
 
 export const Container = styled(SafeAreaView)`
    flex: 1;
+`;
+
+export const Content = styled.View`
    padding: ${({theme}) => theme.spacing.md}px;
+`;
+
+export const Scroll = styled.ScrollView`
+
+  flex: 1
+
 `;
