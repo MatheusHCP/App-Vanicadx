030d408dc52744494984b40b0abf6e548c57ceaf
Matheus Pereira <mat_pereiraa@hotmail.com>
2022-12-27 14:49:13 -0300

    internal: config maps API

--- src/screens/SignedIn/VaccineOnMaps/index.tsx
+++ src/screens/SignedIn/VaccineOnMaps/index.tsx
@@ -27,8 +27,6 @@ return (
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
